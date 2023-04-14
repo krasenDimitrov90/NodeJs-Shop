c2f153d054100eba35727f3fda122d0c2c92c7c0
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-04-14 13:25:08 +0300

    add ejs template engine

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,12 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {
+    pageTitle: 'Shop',
+    path: '/',
+    prods: adminData.products,
+    hasProducts: adminData.products.length
+  });
 });
 
 module.exports = router;
