d585c01fecacf7bf084f47266ea1cd0866eb6765
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-04-18 07:34:51 +0300

    [ADD] storing the user in DB

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 
 const app = express();
@@ -18,6 +19,16 @@ const pageNotFoundControler = require('./controlers/404');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById('643e1b5e7007909c6481a157')
+        .then(user => {
+            req.user = user;
+            console.log(user);
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
+const ObjectId = mongodb.ObjectId;
+
+module.exports = class User {
+    constructor(username, email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users')
+            .findOne({ _id: new ObjectId(userId) });
+    }
+}
